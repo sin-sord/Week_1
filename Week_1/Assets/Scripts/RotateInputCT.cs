dfd6506d0403b3bba45c62a987f09b4fc62550b6
sin-sord <kevkinzz@gmail.com>
2025-01-15 18:00:07 -0500

    Task 3 complete
    Object rotates when SPACE is held down

---
+++ Week_1/Assets/Scripts/RotateInputCT.cs
@@ -0,0 +1,49 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Conditions
+{
+
+    public class RotateInputCT : ConditionTask
+    {
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit()
+        {
+            return null;
+        }
+
+        //Called whenever the condition gets enabled.
+        protected override void OnEnable()
+        {
+
+        }
+
+        //Called whenever the condition gets disabled.
+        protected override void OnDisable()
+        {
+
+        }
+
+        //Called once per frame while the condition is active.
+        //Return whether the condition is success or failure.
+        protected override bool OnCheck()
+        {
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+    }
+}
