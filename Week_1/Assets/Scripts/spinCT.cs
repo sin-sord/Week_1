ff446d9869a2f92c1ff016aeafb157b5743fc095
sin-sord <kevkinzz@gmail.com>
2025-01-23 13:02:49 -0500

    added sprites

---
+++ Week_1/Assets/Scripts/spinCT.cs
@@ -0,0 +1,43 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Conditions {
+
+	public class spinCT : ConditionTask {
+
+		//Use for initialization. This is called only once in the lifetime of the task.
+		//Return null if init was successfull. Return an error string otherwise
+		protected override string OnInit(){
+			return null;
+		}
+
+		//Called whenever the condition gets enabled.
+		protected override void OnEnable() {
+			
+		}
+
+		//Called whenever the condition gets disabled.
+		protected override void OnDisable() {
+			
+		}
+
+		//Called once per frame while the condition is active.
+		//Return whether the condition is success or failure.
+		protected override bool OnCheck() {
+
+			if (Input.GetKeyDown(KeyCode.W))
+			{
+				Debug.Log("the player can spin!");
+				return true;
+
+			}
+			else
+			{
+				return false;
+
+			}
+		}
+	}
+}
